cc53558f9530322d0ef333a8bbb2ddc5c7994272
Exponential Only <jawdat@jawdatshammas.com>
2025-06-23 13:47:54 +0400

    Update get-influencers.js

--- pages/api/get-influencers.js
+++ pages/api/get-influencers.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Replace with your actual Sheet ID from the Google Sheets URL
-  const SHEET_ID = '1ebZswB2W8yT7VhfIhB5wyS2NvreKkC2lq5lmlX1Kp-E';
+  const SHEET_ID = '1Xtl_DofYpaAlAaHlGD7lw4lCs53Mr7yU-rUUN2yo-lA';
   const SHEET_NAME = 'Sheet1'; // Default sheet name
   
   // Valid categories
